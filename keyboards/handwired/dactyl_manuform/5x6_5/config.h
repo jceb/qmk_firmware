424b4c169ce0518acdba8ec7c2273f28b189b591
Jan Christoph Ebersbach <jceb@e-jc.de>
2020-07-05 21:09:01 +0200

    fix: properly set the bootloader key

--- keyboards/handwired/dactyl_manuform/5x6_5/config.h
+++ keyboards/handwired/dactyl_manuform/5x6_5/config.h
@@ -38,8 +38,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BOOTMAGIC_LITE_ROW 0
 #define BOOTMAGIC_LITE_COLUMN 0
 
-#define BOOTMAGIC_LITE_ROW_RIGHT 1
-#define BOOTMAGIC_LITE_COLUMN_RIGHT 0
+#define BOOTMAGIC_LITE_ROW_RIGHT 6
+#define BOOTMAGIC_LITE_COLUMN_RIGHT 5
 
 // WS2812 RGB LED strip input and number of LEDs
 // #define RGB_DI_PIN D3
